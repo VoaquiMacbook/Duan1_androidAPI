c17573ce64aa73157a7b632d670f914dad6a8ce5
Vo-A-Qui <vohqui51202@icloud.com>
2024-03-06 23:08:48 +0700

    ok

---
+++ app/src/main/java/com/example/duan_coffeeshop/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.duan_coffeeshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String qui = "123";
+        
+    }
+}
